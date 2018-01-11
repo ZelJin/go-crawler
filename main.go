91220cb53c80d18a6e5431a95347f3a8617797f0
Dmitry Zeldin <dmitry@zeldin.pro>
2018-01-11 21:08:08 +0100

    Updates usage text and version.

--- main.go
+++ main.go
@@ -11,8 +11,11 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "go-crawler"
 	app.Usage = "Generate a sitemap of a given domain."
+	app.UsageText = "go-crawler [domain]"
+	app.Version = "1.0.0"
 	app.Action = func(c *cli.Context) error {
-		fmt.Println("Crawling...")
+		domain := c.Args().Get(0)
+		fmt.Printf("Crawling %v...\n", domain)
 		return nil
 	}
 
