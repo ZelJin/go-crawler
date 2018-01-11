963dc90efc1d3bbd96ef83bdecc570992edb82be
Dmitry Zeldin <dmitry@zeldin.pro>
2018-01-11 21:01:57 +0100

    Added cli package

--- main.go
+++ main.go
@@ -1,7 +1,20 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"os"
+
+	"github.com/urfave/cli"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	app := cli.NewApp()
+	app.Name = "go-crawler"
+	app.Usage = "Generate a sitemap of a given domain."
+	app.Action = func(c *cli.Context) error {
+		fmt.Println("Crawling...")
+		return nil
+	}
+
+	app.Run(os.Args)
 }
