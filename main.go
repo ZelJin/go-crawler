fe7cdf4ac21f31e525c2dfa680273c11209b84ff
Dmitry Zeldin <dmitry@zeldin.pro>
2018-01-12 00:41:30 +0100

    WIP: first attempt in concurrency

--- main.go
+++ main.go
@@ -6,10 +6,16 @@ import (
 	"net/http"
 	"net/url"
 	"os"
+	"time"
 
 	"github.com/urfave/cli"
 	"golang.org/x/net/html"
 )
+
+type CrawlState struct {
+	Path  string
+	Depth int
+}
 
 // Fetch page using http.Get
 func fetchPage(host, path string) (*http.Response, error) {
@@ -39,22 +45,27 @@ func extractLinks(body io.ReadCloser) (links []string) {
 }
 
 // Crawl a page specified by domain and relative path
-func crawlPage(host, path string, sitemap Sitemap, depth, maxDepth int) error {
+func crawlPage(host string, sitemap Sitemap, state CrawlState, queue chan CrawlState, finished chan bool) error {
 	// Return if this path already exists
-	if _, found := sitemap[path]; found {
+	if _, found := sitemap[state.Path]; found {
 		fmt.Println("Page has already been crawled, skipping.")
 		return nil
 	}
-	if depth == maxDepth {
+	if state.Depth < 0 {
 		fmt.Println("Maximum depth has been reached, skipping.")
+		return nil
 	}
 	// Add page to the global state
-	sitemap[path] = NewStringSet()
+	sitemap[state.Path] = NewStringSet()
 	// Fetch the page
-	res, err := fetchPage(host, path)
+	fmt.Printf("Crawling %v\n", state.Path)
+	res, err := fetchPage(host, state.Path)
 	if err != nil {
 		return err
 	}
+
+	defer func() { finished <- true }()
+
 	// Extract links
 	defer res.Body.Close()
 	links := extractLinks(res.Body)
@@ -65,9 +76,9 @@ func crawlPage(host, path string, sitemap Sitemap, depth, maxDepth int) error {
 		}
 		//fmt.Println(url.Hostname())
 		if url.Hostname() == host || url.Hostname() == "" {
-			fmt.Printf("Found link: %v -> %v\n", path, url.Path)
-			sitemap[path].Add(url.Path)
-			crawlPage(host, url.Path, sitemap, depth+1, maxDepth)
+			fmt.Printf("Found link: %v -> %v\n", state.Path, url.Path)
+			sitemap[state.Path].Add(url.Path)
+			queue <- CrawlState{url.Path, state.Depth - 1}
 		}
 	}
 	return nil
@@ -79,7 +90,7 @@ func main() {
 	app := cli.NewApp()
 	app.Name = "go-crawler"
 	app.Usage = "Generate a sitemap for a given hostname."
-	app.UsageText = "go-crawler [hostname]"
+	app.UsageText = "go-crawler [-d | --depth] [hostname]"
 	app.Version = "1.0.0"
 	app.Flags = []cli.Flag{
 		cli.IntFlag{
@@ -96,16 +107,35 @@ func main() {
 			return nil
 		}
 		host := c.Args().Get(0)
-		fmt.Printf("Crawling %v...\n", host)
-		// Create a global storage for pages
-		// State is stored in a map, where keys are relative paths,
-		// and value is a slice of links to other pages.
+		fmt.Printf("Started crawling %v\n", host)
+
+		// Start recording time
+		start := time.Now()
+
 		sitemap := Sitemap{}
-		err := crawlPage(host, "", sitemap, 0, depth)
-		if err != nil {
-			fmt.Println(err)
+
+		queue := make(chan CrawlState)
+		finished := make(chan bool)
+		openRoutines := 1
+		finishedRoutines := 0
+
+		go crawlPage(host, sitemap, CrawlState{"", depth}, queue, finished)
+
+		for finishedRoutines < openRoutines {
+			select {
+			case state := <-queue:
+				openRoutines++
+				go crawlPage(host, sitemap, state, queue, finished)
+			case <-finished:
+				finishedRoutines++
+			}
 		}
+
 		sitemap.Print()
+
+		elapsed := time.Since(start)
+		fmt.Printf("Crawling took %s\n", elapsed)
+
 		return nil
 	}
 
