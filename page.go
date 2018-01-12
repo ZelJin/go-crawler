1a5aef4de035ad3e1964b6473b89672b88b8d2db
Dmitry Zeldin <dmitry@zeldin.pro>
2018-01-12 20:23:45 +0100

    Updated docs for Page

--- page.go
+++ page.go
@@ -7,37 +7,40 @@ import (
 	"sync"
 )
 
+// Page is a thread-safe implementation of a node in a sitemap graph.
+// Page has a URL and a list of links to other pages.
 type Page struct {
 	sync.Mutex
 	URL   *url.URL
 	Links []*Page
 }
 
+// NewPage initializes a Page object with a given URL.
 func NewPage(url *url.URL) *Page {
 	return &Page{URL: url, Links: []*Page{}}
 }
 
-// PrintSitemap function prints a sitemap for a given page
+// PrintSitemap function prints a sitemap for a given page.
 func (p *Page) PrintSitemap() {
 	fmt.Println("Printing sitemap...")
-	// Generate another map to track visited nodes
+	// Generate a map to track visited nodes
 	visited := map[string]bool{}
 	traverse(p, "", visited, 0)
 }
 
+// Traverse a node in a sitemap graph. Used in PrintSitemap.
 func traverse(p *Page, prefix string, visited map[string]bool, depth int) {
 	// Check if the node has been visited
 	if _, found := visited[p.URL.String()]; found {
 		return
 	}
-	// Set node to visited
+	// Mark node as visited
 	visited[p.URL.String()] = true
-	// Print the node, w.r.t prefix
+	// Print the URL of the node with its prefix
 	prefix = strings.Repeat("  ", depth) + prefix
 	fmt.Println(prefix, p.URL.String())
 	// Run traverse for every child
-	links := p.Links
-	for _, child := range links {
+	for _, child := range p.Links {
 		traverse(child, " ├──", visited, depth+1)
 	}
 }
