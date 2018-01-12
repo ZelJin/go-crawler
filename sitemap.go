4915890f4ea414b7b0e2228c9ba6c59f21d7b40a
Dmitry Zeldin <dmitry@zeldin.pro>
2018-01-12 01:22:00 +0100

    Implemented thread-safe sitemap

--- sitemap.go
+++ sitemap.go
@@ -1,15 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"sync"
+)
 
-// Sitemap is a map with page URLs as keys and a set of links
+// Sitemap is thread-safe a map with page URLs as keys and a set of links
 // originating from a particular page as values
-type Sitemap map[string]Page
+type Sitemap struct {
+	sync.Mutex
+	sitemap map[string]Page
+}
+
+func (s Sitemap) Set(key string, value Page) {
+	s.Lock()
+	defer s.Unlock()
+	s.sitemap[key] = value
+}
+
+func (s Sitemap) Get(key string) (Page, bool) {
+	s.Lock()
+	defer s.Unlock()
+	return s.sitemap[key]
+}
+
+func (s Sitemap) GetSitemap() map[string]Page {
+	s.Lock()
+	defer s.Unlock()
+	return s.sitemap
+}
 
 // Print function prints a sitemap
 func (s Sitemap) Print() {
+	s.Lock()
+	defer s.Unlock()
 	fmt.Println("Sitemap:")
-	for _, page := range s {
+	for _, page := range s.Sitemap {
 		fmt.Println(page.Path)
 		for i, link := range page.LinkSet.List() {
 			symbol := "├── "
